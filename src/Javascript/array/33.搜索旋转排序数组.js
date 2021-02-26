f48dc6c6c01ac3c795e63fd0da02951f7569f551
joanna <qiqiai77a@126.com>
2021-02-26 15:45:31 +0800

    finish javascript version of array 33

--- src/Javascript/array/33.搜索旋转排序数组.js
+++ src/Javascript/array/33.搜索旋转排序数组.js
@@ -13,16 +13,19 @@
 var search = function(nums, target) {
     let left = 0, right = nums.length - 1;
     const maxNumber = Math.pow(10, 4);
+    // ! =
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
         if (nums[mid] === target) return mid;
         // judge target locate at which side of the array
         if (target >= nums[0]) {
             // judge mid locate at which side of the array
-            if (nums[mid] < nums[0]) nums[mid] = maxNumber;
+            if (nums[mid] < nums[0]) nums[mid] = maxNumber; // sort
         } else {
-            if (nums[mid] >= nums[0]) nums[mid] = -maxNumber;
+            // ! =
+            if (nums[mid] >= nums[0]) nums[mid] = -maxNumber; // sort
         }
+        // binary search
         if (nums[mid] > target) {
             right = mid - 1;
         } else {
