6fb63811f6108f6139da2d1b72535371a2d77902
joanna <qiqiai77a@126.com>
2021-02-26 17:37:19 +0800

    finish java version of array 34

---
+++ src/Javascript/array/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=javascript
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+
+};
+// @lc code=end
+
