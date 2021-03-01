b1e762d285aa6b6e32a67dc941028cc153d838bb
joanna <qiqiai77a@126.com>
2021-03-01 17:21:47 +0800

    finish python version of array 42

--- src/Python/array/42.接雨水.py
+++ src/Python/array/42.接雨水.py
@@ -11,5 +11,20 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        if height == None or len(height) == 0:
+            return 0
+        length = len(height)
+        ans = 0
+        left_max = [0] * length
+        right_max = [0] * length
+        left_max[0] = height[0]
+        for i in range(1, length):
+            left_max[i] = max(height[i], left_max[i - 1])
+        right_max[length - 1] = height[length - 1]
+        for i in range(length - 2, -1, -1):
+            right_max[i] = max(height[i], right_max[i + 1])
+        for i in range(length):
+            ans += min(left_max[i], right_max[i]) - height[i]
+        return ans
 # @lc code=end
 
