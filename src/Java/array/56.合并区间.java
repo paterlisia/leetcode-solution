58b4d11106ba550c6d6d35b69393d9d72d547158
joanna <qiqiai77a@126.com>
2021-03-03 14:54:33 +0800

    comment changes and add array 56 java version

---
+++ src/Java/array/56.合并区间.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=java
+ *
+ * [56] 合并区间
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+    }
+}
+// @lc code=end
+
