bfc192124e885e54109c8c86d0e666f7a5280874
joanna <qiqiai77a@126.com>
2021-03-05 15:22:34 +0800

    finish java version of array 75

---
+++ src/Java/array/75.颜色分类.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=75 lang=java
+ *
+ * [75] 颜色分类
+ */
+
+// @lc code=start
+class Solution {
+    public void sortColors(int[] nums) {
+        int p0 = 0, p2 = nums.length - 1;
+        // double pointer
+        for (int i = 0; i <= p2; i++) {
+            while (i <= p2 && nums[i] == 2) swap(nums, i , p2--);
+            if (nums[i] == 0) swap(nums, i, p0++);
+        }
+    }
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+// @lc code=end
+
