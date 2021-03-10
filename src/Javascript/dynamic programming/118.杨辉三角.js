160d8387625523e3481b236efdc9d21e1f9b233a
joanna <qiqiai77a@126.com>
2021-03-10 11:04:06 +0800

    finish javascript version of array 118

--- src/Javascript/dynamic programming/118.杨辉三角.js
+++ src/Javascript/dynamic programming/118.杨辉三角.js
@@ -12,9 +12,12 @@
 var generate = function (numRows) {
     const res = [];
     for (let i = 0; i < numRows; i++) {
+        // number list in current row
         const level = [];
         for (let j = 0; j < i + 1; j++) {
+            // boarder condition
             if (j === 0 || j === i) level.push(1);
+            // transfer equation (j = 0 when i = 0 so we can ignore i = 0)
             else level.push(res[i - 1][j - 1] + res[i - 1][j]);
         }
         res.push(level);
