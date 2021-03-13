5ca82ed8ec29121144ca06c8b238a3bdfbc6eb8f
joanna <qiqiai77a@126.com>
2021-03-13 10:45:43 +0800

    finish python version of linked list 206

---
+++ src/Javascript/Linked-List/206.反转链表.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=206 lang=python
+#
+# [206] 反转链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre = None
+        cur = head
+        while cur:
+            next = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next
+        return pre
+# @lc code=end
+
