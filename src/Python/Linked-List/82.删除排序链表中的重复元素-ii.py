ff9bbe8531b65f761596abd89df5cb289fd84e7b
joanna <qiqiai77a@126.com>
2021-03-13 12:41:37 +0800

    finish python version of linked list 82

---
+++ src/Python/Linked-List/82.删除排序链表中的重复元素-ii.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=82 lang=python
+#
+# [82] 删除排序链表中的重复元素 II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        if head.val == head.next.val:
+            while head and head.next and head.val == head.next.val:
+                head = head.next
+            return self.deleteDuplicates(head.next)
+        else:
+            head.next = self.deleteDuplicates(head.next)
+            return head
+# @lc code=end
+
