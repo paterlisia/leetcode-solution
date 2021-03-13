518f0a2f4cfb7561d11cae3939f03160752781a8
joanna <qiqiai77a@126.com>
2021-03-13 10:47:46 +0800

    finish java version of linked list 206

---
+++ src/Javascript/Linked-List/206.反转链表.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+// @lc code=end
+
