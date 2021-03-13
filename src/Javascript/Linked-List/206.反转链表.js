202085ac864cbee90ef7efee365d825eafa362ea
joanna <qiqiai77a@126.com>
2021-03-13 10:50:52 +0800

    finish javascript version of linked list 206

---
+++ src/Javascript/Linked-List/206.反转链表.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let pre = null, cur = head;
+    while (cur !== null) {
+        let next = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
+};
+// @lc code=end
+
