7a46727eaa30077d143ea10f9fe2eb5080dab25c
joanna <qiqiai77a@126.com>
2021-03-14 19:36:28 +0800

    finish javascript version of tree 100

---
+++ src/Javascript/tree/100.相同的树.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=javascript
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if (p === null && q === null) return true;
+    if (p === null) return false;
+    if (q === null) return false;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+// @lc code=end
+
