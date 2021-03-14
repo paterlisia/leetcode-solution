78dc92dfad61068f6fd2cef0dfd1c994f9353cb1
joanna <qiqiai77a@126.com>
2021-03-14 08:08:01 +0800

    finish python version of tree 98

---
+++ src/Javascript/tree/98.验证二叉搜索树.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=98 lang=python
+#
+# [98] 验证二叉搜索树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack = []
+        pre = float('-inf')
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if root.val <= pre:
+                return False
+            pre = root.val
+            root = root.right
+        return True
+# @lc code=end
+
