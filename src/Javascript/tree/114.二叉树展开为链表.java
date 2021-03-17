9f154cb78a22e907c6ba9bd7d358ca41ca58c53b
joanna <qiqiai77a@126.com>
2021-03-17 22:46:24 +0800

    finish javascript version of tree 114

--- src/Javascript/tree/114.二叉树展开为链表.java
+++ src/Javascript/tree/114.二叉树展开为链表.java
@@ -22,7 +22,25 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-
+        while (root != null) { 
+            //左子树为 null，直接考虑下一个节点
+            if (root.left == null) {
+                root = root.right;
+            } else {
+                // 找左子树最右边的节点
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                } 
+                //将原来的右子树接到左子树的最右边节点
+                pre.right = root.right;
+                // 将左子树插入到右子树的地方
+                root.right = root.left;
+                root.left = null;
+                // 考虑下一个节点
+                root = root.right;
+            }
+        }
     }
 }
 // @lc code=end
