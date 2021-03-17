575931b93098661169a5965e3897ab96fe9b5802
joanna <qiqiai77a@126.com>
2021-03-17 22:45:13 +0800

    finish javascript version of tree 114

---
+++ src/Javascript/tree/114.二叉树展开为链表.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=java
+ *
+ * [114] 二叉树展开为链表
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+
+    }
+}
+// @lc code=end
+
