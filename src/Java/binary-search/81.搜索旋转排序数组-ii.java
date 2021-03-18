0b16f520c9ca7922252a500437c42907e518ca92
joanna <qiqiai77a@126.com>
2021-03-18 16:55:16 +0800

    finish java version of binary search 81

---
+++ src/Java/binary-search/81.搜索旋转排序数组-ii.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=81 lang=java
+ *
+ * [81] 搜索旋转排序数组 II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[left] == nums[mid]) {
+                left++;
+                continue;
+            }
+            if (nums[left] < nums[mid]) {
+                if (nums[mid] > target && nums[left] <= target) right = mid - 1;
+                else left = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
