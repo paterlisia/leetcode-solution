3073b1c773645e2aebc3aaa0c5a46d7f6cc0540b
joanna <qiqiai77a@126.com>
2021-03-20 19:14:31 +0800

    finish java version of dynamic programming 62

---
+++ src/Java/two-pointers/62.不同路径.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=java
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) 
+                dp[j] += dp[j - 1];
+        }
+        return dp[n - 1];
+    }
+}
+// @lc code=end
+
