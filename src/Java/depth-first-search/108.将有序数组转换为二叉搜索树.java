f76647ba693b3c5c3ba6f93e0122e3b099bb89f4
joanna <qiqiai77a@126.com>
2021-03-24 16:23:08 +0800

    finish java version of depth first search 108

---
+++ src/Java/depth-first-search/108.将有序数组转换为二叉搜索树.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=java
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+    public TreeNode dfs(int[] nums, int lo, int hi) {
+        if (lo > hi) return null;
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, lo, mid - 1);
+        root.right = dfs(nums, mid + 1, hi);
+        return root;
+    }
+}
+// @lc code=end
+
