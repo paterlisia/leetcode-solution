ca5ee491d714603acab6115f670982d354860cbb
joanna <qiqiai77a@126.com>
2021-03-24 15:15:35 +0800

    finish java version of depth first search 98

---
+++ src/Java/depth-first-search/98.验证二叉搜索树.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        // long
+        long pre = Long.MIN_VALUE;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= pre) return false;
+            pre = root.val;
+            root = root.right;
+        }
+    return true;
+    }
+}
+// @lc code=end
+
