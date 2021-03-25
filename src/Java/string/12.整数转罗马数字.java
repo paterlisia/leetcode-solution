4db546b97a9ee8a8b72345873b8d60fa3172ade7
joanna <qiqiai77a@126.com>
2021-03-25 14:54:06 +0800

    finish java version of string 12

---
+++ src/Java/string/12.整数转罗马数字.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=12 lang=java
+ *
+ * [12] 整数转罗马数字
+ */
+
+// @lc code=start
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};    
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                res.append(symbols[i]);
+            }
+        }
+        return res.toString();
+    }
+}
+// @lc code=end
+
