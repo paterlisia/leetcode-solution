a93cc795bfa9b046c7e96c2b6a9a947568b6722b
joanna <qiqiai77a@126.com>
2021-03-27 23:31:15 +0800

    finish java version of string 38

--- src/Java/string/32.最长有效括号.java
+++ src/Java/string/32.最长有效括号.java
@@ -13,7 +13,7 @@ class Solution {
             if (s.charAt(i) == ')') {
                 if (s.charAt(i - 1) == '(') dp[i] = ((i - 2) >= 0 ? dp[i - 2] : 0) + 2;
                 else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(')
-                    dp[i] = (dp[i - 1] + (i - dp[i - 1] >= 2 ? dp[i - 2] : 0) + 2);
+                    dp[i] = (dp[i - 1] + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2);
             }
             maxLen = Math.max(maxLen, dp[i]);
         }
