120a6d896717170eede75c397a9648b70484eb7b
joanna <qiqiai77a@126.com>
2021-03-28 23:51:41 +0800

    finish java version of string 93

--- src/Java/string/93.复原-ip-地址.java
+++ src/Java/string/93.复原-ip-地址.java
@@ -8,7 +8,7 @@
 class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<String>();
-        backtracking(s, 1, 0, res, new ArrayDeque<String>());
+        backtracking(s, 0, 0, res, new ArrayDeque<String>());
         return res;
     }
     public int isValid(String s, int start, int end) {
@@ -32,10 +32,10 @@ class Solution {
         if (s.length() - start < 4- split || s.length() - start > 3 * (4 - split)) return;
         for (int i = 0; i < 3; i++) {
             if (start + i >= s.length()) break;
-            String substring = isValid(s, start, start + i);
+            int substring = isValid(s, start, start + i);
             if (substring != -1) {
                 path.offerLast(substring + "");
-                backtracking(s, split + 1, start + i, res, path);
+                backtracking(s, split + 1, start + i + 1, res, path);
                 path.removeLast();
             }
         }
