984cdd970111307de3a4c94f46c4c4cce1458d6d
joanna <qiqiai77a@126.com>
2021-03-30 08:41:03 +0800

    finish java version of math 2

---
+++ src/Java/math/2.两数相加.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=java
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode node = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            node.next = new ListNode(sum % 10);
+            l1 = l1 == null ? l1 : l1.next;
+            l2 = l2 == null ? l2 : l2.next;
+            node = node.next;
+        }
+        if (carry == 1) {
+            node.next = new ListNode(1);
+        }
+        return pre.next;
+    }
+}
+// @lc code=end
+
