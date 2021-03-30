aab995a7910aa0b763f48361346099e187bbdd53
joanna <qiqiai77a@126.com>
2021-03-30 11:23:18 +0800

    finish java version of hash table 3

---
+++ src/Java/hash-table/3.无重复字符的最长子串.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=java
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int maxLen = 0, start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                start = Math.max(start, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            maxLen = Math.max(maxLen, i - start + 1);
+        }
+        return maxLen;
+    }
+}
+// @lc code=end
+
