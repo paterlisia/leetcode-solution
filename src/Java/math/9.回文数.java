70608f3443f99368a5af095ca8f88f66c98624c4
joanna <qiqiai77a@126.com>
2021-03-30 09:13:34 +0800

    finish java version of math 9

---
+++ src/Java/math/9.回文数.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=java
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10; 
+            x /= 10;
+        }
+        return rev == x || rev / 10 == x;
+    }
+}
+// @lc code=end
+
