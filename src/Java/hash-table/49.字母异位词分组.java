c5bc14bd59e40fa45314e9da65023e82cd4b6fdf
joanna <qiqiai77a@126.com>
2021-03-30 13:24:42 +0800

    finish java version of hash table 49

---
+++ src/Java/hash-table/49.字母异位词分组.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=java
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str: strs) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+// @lc code=end
+
