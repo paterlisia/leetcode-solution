23a5eaa850bd8d543c6cde66c4c55e1d52694d66
joanna <qiqiai77a@126.com>
2021-04-07 15:59:43 +0800

    finish java version of amazon

---
+++ src/Java/Amazon/23.合并k个升序链表.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return partition(lists, 0, lists.length - 1);
+    }
+    public ListNode partition(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = partition(lists, left, mid);
+        ListNode l2 = partition(lists, mid + 1, right);
+        return sort(l1, l2);
+    }
+    public ListNode sort(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val > l2.val) {
+            l2.next = sort(l1, l2.next);
+            return l2;
+        } else {
+            l1.next = sort(l1.next, l2);
+            return l1;
+        }
+    }
+}
+// @lc code=end
+
