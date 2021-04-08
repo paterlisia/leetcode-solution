73238abfa7dff4334d57a7d1388dcebf4513f0ed
joanna <qiqiai77a@126.com>
2021-04-08 17:32:38 +0800

    finish java version of amazon

---
+++ src/Java/Amazon/102.二叉树的层序遍历.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=java
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+    }
+}
+// @lc code=end
+
--- src/Java/Amazon/89.格雷编码.java
+++ src/Java/Amazon/89.格雷编码.java
@@ -7,7 +7,16 @@
 // @lc code=start
 class Solution {
     public List<Integer> grayCode(int n) {
-
+        List<Integer> res = new ArrayList<Integer>();
+        res.add(0);
+        int head = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = res.size() - 1; j >= 0; j--) {
+                res.add(head + res.get(j));
+            }
+            head <<= 1;
+        }
+        return res;
     }
 }
 // @lc code=end
