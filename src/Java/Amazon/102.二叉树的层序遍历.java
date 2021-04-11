1e5c7644e409b65adc1a557dc904974452038eb6
joanna <qiqiai77a@126.com>
2021-04-11 13:13:06 +0800

    finish java version of amazon

--- src/Java/Amazon/102.二叉树的层序遍历.java
+++ src/Java/Amazon/102.二叉树的层序遍历.java
@@ -22,7 +22,22 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Deque<TreeNode> q = new ArrayDeque<TreeNode>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int currentLength = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < currentLength; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            res.add(level);
+        }
+        return res;
     }
 }
 // @lc code=end
