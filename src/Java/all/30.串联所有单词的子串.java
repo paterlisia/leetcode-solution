58365b52e730fae7dab94c170f001b93711d93ac
joanna <qiqiai77a@126.com>
2021-04-14 16:11:05 +0800

    finish java version of difficult 30

---
+++ src/Java/all/30.串联所有单词的子串.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=30 lang=java
+ *
+ * [30] 串联所有单词的子串
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        int wordNum = words.length;
+        if (wordNum == 0) {
+            return res;
+        }
+        int wordLen = words[0].length();
+        //HashMap1 存所有单词
+        HashMap<String, Integer> allWords = new HashMap<String, Integer>();
+        for (String w : words) {
+            int value = allWords.getOrDefault(w, 0);
+            allWords.put(w, value + 1);
+        }
+        //遍历所有子串
+        for (int i = 0; i < s.length() - wordNum * wordLen + 1; i++) {
+            //HashMap2 存当前扫描的字符串含有的单词
+            HashMap<String, Integer> hasWords = new HashMap<String, Integer>();
+            int num = 0;
+            //判断该子串是否符合
+            while (num < wordNum) {
+                String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
+                //判断该单词在 HashMap1 中
+                if (allWords.containsKey(word)) {
+                    int value = hasWords.getOrDefault(word, 0);
+                    hasWords.put(word, value + 1);
+                    //判断当前单词的 value 和 HashMap1 中该单词的 value
+                    if (hasWords.get(word) > allWords.get(word)) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                num++;
+            }
+            //判断是不是所有的单词都符合条件
+            if (num == wordNum) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
