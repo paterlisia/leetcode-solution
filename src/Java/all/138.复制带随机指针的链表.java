fbe5cf4500febbda611c62e7ed9bf6ae3cb6823f
joanna <qiqiai77a@126.com>
2021-05-13 19:56:32 +0800

    finish java version of 138

---
+++ src/Java/all/138.复制带随机指针的链表.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=java
+ *
+ * [138] 复制带随机指针的链表
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Node, Node> map = new HashMap<Node, Node>();
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        if (max.containsKey(head)) return map.get(head);
+        Node newNode = new Node(head.val);
+        map.put(head, newNode);
+        newNode.next = copyRandomList(head.next);
+        newNode.random = copyRandomList(head.random);
+        return newNode;
+    }
+}
+// @lc code=end
+
