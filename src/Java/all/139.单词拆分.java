d03b59a8b0da3eaf45eb840dc5a9bd2e3ba34b1f
joanna <qiqiai77a@126.com>
2021-05-14 10:34:17 +0800

    finish java version of 139

---
+++ src/Java/all/139.单词拆分.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=java
+ *
+ * [139] 单词拆分
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> word = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && word.contains(s.substring(j, i)))
+                    dp[i] = true;
+            }
+        }
+        return dp[s.length()];
+    }
+}
+// @lc code=end
+
