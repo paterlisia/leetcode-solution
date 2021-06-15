ac709be5981962e65c4ee8f68c4c3c448472483b
joanna <qiqiai77a@126.com>
2021-06-15 11:46:09 +0800

    finish java version of first practice: 116

---
+++ src/Java/practice/first-time/116.填充每个节点的下一个右侧节点指针.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=java
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node nodeStart = root;
+        if (root == null) return root;
+        while (nodeStart.left != null) {
+            Node node = nodeStart;
+            while (node != null)
+            {
+                node.left.next = node.right;
+            if (node.next != null) {
+                node.right.next = node.next.left;
+            }
+            node = node.next;
+            }
+            nodeStart = nodeStart.left;
+        }
+        return root;
+    }
+}
+// @lc code=end
+
