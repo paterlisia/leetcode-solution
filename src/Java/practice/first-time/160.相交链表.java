216d1f15a3046b2a2b16b7d933912f1bb13af233
joanna <qiqiai77a@126.com>
2021-06-26 21:59:32 +0800

    finish java version of first practice: 160

---
+++ src/Java/practice/first-time/160.相交链表.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=java
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode l1 = headA, l2 = headB;
+        while (l1 != l2) {
+            if (l1 == null) l1 = headB;
+            else l1 = l1.next;
+            if (l2 == null) l2 = headA;
+            else l2 = l2.next;
+        }
+        return l1;
+    }
+}
+// @lc code=end
+
