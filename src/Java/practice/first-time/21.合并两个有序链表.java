ffbe3f8f10a10607daa860d911e697eca98d58c2
joanna <qiqiai77a@126.com>
2021-07-06 15:40:23 +0800

    finish java version of first practice: 21

---
+++ src/Java/practice/first-time/21.合并两个有序链表.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                dummy.next = l1;
+                l1 = l1.next;
+            } else {
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+        dummy.next = l1 != null? l1 : l2;
+        return head.next;
+    }
+}
+// @lc code=end
+
