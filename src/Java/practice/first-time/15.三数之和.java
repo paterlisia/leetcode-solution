949dd765b47fe1f604ec56fad95f3f3746a14540
joanna <qiqiai77a@126.com>
2021-07-06 14:26:25 +0800

    finish java version of first practice: 14

---
+++ src/Java/practice/first-time/15.三数之和.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int lo = i + 1, hi = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > 0) return res;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                    lo++;
+                    hi--;
+                }
+                else if (sum > 0) hi--;
+                else lo++;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
