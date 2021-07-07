9339b090367870bb48373fd8515264507d8b7089
joanna <qiqiai77a@126.com>
2021-07-07 15:19:02 +0800

    finish java version of first practice: 41

---
+++ src/Java/practice/first-time/41.缺失的第一个正数.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=41 lang=java
+ *
+ * [41] 缺失的第一个正数
+ */
+
+// @lc code=start
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i])
+                swap(nums, nums[i] - 1, i);
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return len + 1;
+    }
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+// @lc code=end
+
