f141eb9cb18ff15805c9033bcfbe3f8e7b314d07
joanna <qiqiai77a@126.com>
2021-07-07 11:16:35 +0800

    finish java version of first practice: 35

---
+++ src/Java/practice/first-time/35.搜索插入位置.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+}
+// @lc code=end
+
