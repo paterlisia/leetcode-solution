4ae893283abdc382146e8af672126656751b82e2
joanna <qiqiai77a@126.com>
2021-07-08 15:43:27 +0800

    finish java version of first practice: 73

---
+++ src/Java/practice/first-time/74.搜索二维矩阵.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=java
+ *
+ * [74] 搜索二维矩阵
+ */
+
+// @lc code=start
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+    }
+}
+// @lc code=end
+
