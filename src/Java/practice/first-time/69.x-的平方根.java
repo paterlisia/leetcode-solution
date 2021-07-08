8d7e513d4f5a9f7f147759a8dcea9375cac04430
joanna <qiqiai77a@126.com>
2021-07-08 14:49:59 +0800

    finish java version of first practice: 69

---
+++ src/Java/practice/first-time/69.x-的平方根.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=java
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int x) {
+        long res = x;
+        while (res * res > x) {
+            res = (res + x / res) / 2;
+        }
+        return (int)res;
+    }
+}
+// @lc code=end
+
