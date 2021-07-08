ba1e582822200815f17cf15c977ce3df4c68d74a
joanna <qiqiai77a@126.com>
2021-07-08 09:00:12 +0800

    finish java version of first practice: 50

---
+++ src/Java/practice/first-time/50.pow-x-n.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+    public double myPow(double x, int n) {
+        double res = 1;
+        boolean flag = n > 0 ? true : false;
+        long absn = Math.abs((long)n);
+        while (absn != 0) {
+            if (absn % 2 != 0) {
+                res *= x;
+            }
+            x *= x;
+            absn /= 2;
+        }
+        return flag? res : 1 / res;
+    }
+}
+// @lc code=end
+
