ee9a5e46cc67ee6656d4568abe3a233c01c431d8
joanna <qiqiai77a@126.com>
2021-07-08 15:51:08 +0800

    finish java version of first practice: 74

--- src/Java/practice/first-time/74.搜索二维矩阵.java
+++ src/Java/practice/first-time/74.搜索二维矩阵.java
@@ -7,7 +7,15 @@
 // @lc code=start
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-
+        int lo = 0, hi = matrix.length * matrix[0].length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int y = mid % matrix[0].length, x = mid / matrix[0].length;
+            if (target == matrix[x][y]) return true;
+            else if (target < matrix[x][y]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return false;
     }
 }
 // @lc code=end
