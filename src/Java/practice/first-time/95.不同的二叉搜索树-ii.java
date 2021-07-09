5bbe2edb1f2c3ed03a0cacc54f7cac3218362cdf
joanna <qiqiai77a@126.com>
2021-07-09 17:20:32 +0800

    finish java version of first practice: 95

---
+++ src/Java/practice/first-time/95.不同的二叉搜索树-ii.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=95 lang=java
+ *
+ * [95] 不同的二叉搜索树 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<TreeNode>();
+        return getBinarySearchTree(1, n);
+    }
+    public List<TreeNode> getBinarySearchTree(int start, int end) {
+        List<TreeNode> res = new ArrayList<TreeNode>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        if (start == end) {
+            res.add(new TreeNode(start));
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = new ArrayList<TreeNode>();
+            List<TreeNode> rightTrees = new ArrayList<TreeNode>();
+            leftTrees = getBinarySearchTree(start, i - 1);
+            rightTrees = getBinarySearchTree(i + 1, end);
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+
+        }
+        return res;
+    }
+}
+// @lc code=end
+
