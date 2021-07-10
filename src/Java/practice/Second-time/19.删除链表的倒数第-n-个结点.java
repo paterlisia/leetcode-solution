e10fa430a6ca3ade0e5fb949650a473e03f7b5bb
joanna <qiqiai77a@126.com>
2021-07-10 17:55:30 +0800

    finish java version of second practice: 19

---
+++ src/Java/practice/Second-time/19.删除链表的倒数第-n-个结点.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=java
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy, fast = dummy;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
+// @lc code=end
+
