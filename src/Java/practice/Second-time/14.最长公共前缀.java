c9b1615a93e9544322f60deecd6432924accb5fa
joanna <qiqiai77a@126.com>
2021-07-10 17:33:03 +0800

    finish java version of second practice: 14

---
+++ src/Java/practice/Second-time/14.最长公共前缀.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=java
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i))
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+}
+// @lc code=end
+
