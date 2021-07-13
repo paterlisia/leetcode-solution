f8accd710f36f804357004cc063941bd5e147b1c
joanna <qiqiai77a@126.com>
2021-07-13 10:03:44 +0800

    finish java version of second practice: 32

---
+++ src/Java/practice/Second-time/32.最长有效括号.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=java
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxLen = 0;
+        if (s.length() == 0) return maxLen;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i - 2 >= 0 ? dp[i - 2] : 0 ) + 2;
+                } else {
+                    if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = (i - dp[i - 1] - 1 >= 2 ? dp[i - dp[i -1] - 2] : 0) + 2 + dp[i - 1];
+                    }
+                }
+                maxLen = Math.max(maxLen, dp[i]);
+            }
+        }
+        return maxLen;
+    }
+}
+// @lc code=end
+
