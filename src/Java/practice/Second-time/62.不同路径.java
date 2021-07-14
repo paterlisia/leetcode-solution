37786d20def1dafd58127036e09da33d92ce70be
joanna <qiqiai77a@126.com>
2021-07-14 10:14:45 +0800

    finish java version of second practice: 62

--- src/Java/practice/Second-time/62.不同路径.java
+++ src/Java/practice/Second-time/62.不同路径.java
@@ -7,7 +7,14 @@
 // @lc code=start
 class Solution {
     public int uniquePaths(int m, int n) {
-
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
     }
 }
 // @lc code=end
