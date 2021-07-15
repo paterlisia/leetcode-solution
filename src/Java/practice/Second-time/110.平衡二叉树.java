edc1f514a7642b81890f58ffc9a94ea9074215d9
joanna <qiqiai77a@126.com>
2021-07-15 17:34:30 +0800

    finish java version of second practice: 109

---
+++ src/Java/practice/Second-time/110.平衡二叉树.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=java
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+
+    }
+}
+// @lc code=end
+
