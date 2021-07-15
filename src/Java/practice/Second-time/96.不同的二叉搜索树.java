73e9411dc97269d5dc9a382e5a1e473ae479b529
joanna <qiqiai77a@126.com>
2021-07-15 11:55:45 +0800

    finish java version of second practice: 96

---
+++ src/Java/practice/Second-time/96.不同的二叉搜索树.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=96 lang=java
+ *
+ * [96] 不同的二叉搜索树
+ */
+
+// @lc code=start
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[i - j] * dp[j - 1];
+            }
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
+
