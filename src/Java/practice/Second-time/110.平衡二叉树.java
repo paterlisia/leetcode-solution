abd1c35858842c25f1c63c141ad648116361e7ba
joanna <qiqiai77a@126.com>
2021-07-15 17:39:17 +0800

    finish java version of second practice: 110

--- src/Java/practice/Second-time/110.平衡二叉树.java
+++ src/Java/practice/Second-time/110.平衡二叉树.java
@@ -22,7 +22,15 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-
+        return height(root) == -1 ? false : true;
+    }
+    public int height (TreeNode root) {
+        if (root == null) return 0;
+        int left = height(root.left);
+        if (left == -1) return -1;
+        int right = height(root.right);
+        if (right == -1) return -1;
+        return Math.abs(right - left) < 2 ? 1 + Math.max(right, left) : -1;
     }
 }
 // @lc code=end
