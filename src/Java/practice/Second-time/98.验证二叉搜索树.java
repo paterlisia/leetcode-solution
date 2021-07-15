56d861fe08fd2c70bbdc9f7f47ece947718aa966
joanna <qiqiai77a@126.com>
2021-07-15 14:56:27 +0800

    finish java version of second practice: 98

---
+++ src/Java/practice/Second-time/98.验证二叉搜索树.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        int pre = Integer.MIN_VALUE;
+        Stack<TreeNode> s = new Stack<>();
+        while (root != null || !s.isEmpty()) {
+            while (root != null) {
+                s.push(root);
+                root = root.left;
+            }
+            root = s.pop();
+            if (root.val <= pre) return false;
+            pre = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
