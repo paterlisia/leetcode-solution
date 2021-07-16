81eb2d8ad061828ba4436d2fee35f58a3890f3e4
joanna <qiqiai77a@126.com>
2021-07-16 11:01:23 +0800

    finish java version of second practice: 121

---
+++ src/Java/practice/Second-time/121.买卖股票的最佳时机.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=java
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxPro = 0;
+        int minPri = prices[0];
+        for (int p : prices) {
+            minPri = Math.min(p, minPri);
+            maxPro = Math.max(maxPro, p - minPri);
+        }
+        return maxPro;
+    }
+}
+// @lc code=end
+
