4af3d12df8750b5d6467c277b28b61f167d9ab8f
joanna <qiqiai77a@126.com>
2021-07-16 11:17:06 +0800

    finish java version of second practice: 129

---
+++ src/Java/practice/Second-time/129.求根节点到叶节点数字之和.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=129 lang=java
+ *
+ * [129] 求根节点到叶节点数字之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return backtracking(root, 0);
+    }
+    public int backtracking (TreeNode root, int pre) {
+        if (root == null) return 0;
+        int sum = pre * 10 + root.val;
+        if (root.left == null && root.right == null) return sum;
+        return backtracking(root.left, sum) + backtracking(root.right, sum);
+    }
+}
+// @lc code=end
+
