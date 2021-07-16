46376a68710a85060bccfc721dccc92c966209d6
joanna <qiqiai77a@126.com>
2021-07-16 09:38:51 +0800

    finish java version of second practice: 113

--- src/Java/practice/Second-time/113.路径总和-ii.java
+++ src/Java/practice/Second-time/113.路径总和-ii.java
@@ -22,7 +22,19 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        backtracking(root, targetSum, res, new ArrayDeque<Integer>());
+        return res;
+    }
+    public void backtracking(TreeNode root, int targetSum, List<List<Integer>> res, Deque<Integer> path) {
+        if (root == null) return;
+        path.offerLast(root.val);
+        targetSum -= root.val;
+        if (root.left == null && root.right == null && targetSum == 0) res.add(new ArrayList<Integer>(path));
+        backtracking(root.left, targetSum, res, path);
+        backtracking(root.right, targetSum, res, path);
+        path.removeLast();
     }
 }
 // @lc code=end
