bc2a2a824c413a73b31176cdd083ade6d75e9034
joanna <qiqiai77a@126.com>
2021-07-19 09:53:25 +0800

    finish java version of second practice: 134

--- src/Java/practice/Second-time/134.加油站.java
+++ src/Java/practice/Second-time/134.加油站.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-
+        for (int i = 0; i < gas.length;) {
+            int count = 0;
+            int totalGas = 0, totalCost = 0;
+            while (count < gas.length) {
+                int index = (i + count) % gas.length;
+                totalCost += cost[index];
+                totalGas += gas[index];
+                if (totalCost > totalGas) break;
+                count++;
+            }
+            if (count == gas.length) return i;
+            i = (i + count) + 1;
+        }
+        return -1;
     }
 }
 // @lc code=end
