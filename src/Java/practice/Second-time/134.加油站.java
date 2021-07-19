9caa1519c382a6edcb0d0fd0a34dc467ba6e5d30
joanna <qiqiai77a@126.com>
2021-07-19 09:42:33 +0800

    finish java version of second practice: 133

---
+++ src/Java/practice/Second-time/134.加油站.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=134 lang=java
+ *
+ * [134] 加油站
+ */
+
+// @lc code=start
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+    }
+}
+// @lc code=end
+
