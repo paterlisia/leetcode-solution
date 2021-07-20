92eb20d85647aed8e4faed22445384df2f5fb241
joanna <qiqiai77a@126.com>
2021-07-20 22:52:51 +0800

    finish java version of second practice: 140

---
+++ src/Java/practice/Second-time/141.环形链表.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
