369093aa6fc444b52accf7af52cdc4a6a1750b38
joanna <qiqiai77a@126.com>
2021-07-21 15:43:25 +0800

    finish java version of second practice: 172

---
+++ src/Java/practice/Second-time/172.阶乘后的零.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=172 lang=java
+ *
+ * [172] 阶乘后的零
+ */
+
+// @lc code=start
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n / 5;
+            n /= 5;
+        }
+        return count;
+    }
+}
+// @lc code=end
+
