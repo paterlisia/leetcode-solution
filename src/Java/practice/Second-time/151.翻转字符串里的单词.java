bbf08b8a3197e7657488a507a48719c46cb2945e
joanna <qiqiai77a@126.com>
2021-07-21 09:46:22 +0800

    finish java version of second practice: 151

---
+++ src/Java/practice/Second-time/151.翻转字符串里的单词.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=java
+ *
+ * [151] 翻转字符串里的单词
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        List<String> splited = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(splited);
+        return String.join(" ", splited);
+    }
+}
+// @lc code=end
+
