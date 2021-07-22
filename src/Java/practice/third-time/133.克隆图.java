cfcf5c9f24637ad1fcbf140777c7fc91affbbb53
joanna <qiqiai77a@126.com>
2021-07-22 14:44:20 +0800

    finish java version of second practice: 134

---
+++ src/Java/practice/third-time/133.克隆图.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=133 lang=java
+ *
+ * [133] 克隆图
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        
+    }
+}
+// @lc code=end
+
