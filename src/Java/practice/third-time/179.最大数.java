0daf1326c559391831c362b8a24d3df977e7dab3
joanna <qiqiai77a@126.com>
2021-07-22 09:00:40 +0800

    finish java version of second practice: 179

---
+++ src/Java/practice/third-time/179.最大数.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=179 lang=java
+ *
+ * [179] 最大数
+ */
+
+// @lc code=start
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] s = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) s[i] = "" + nums[i];
+        Arrays.sort(s, (a, b) -> {
+            String sa = a + b, sb = b + a;
+            return sb.compareTo(sa);
+        });
+        int k = 0;
+        StringBuilder res = new StringBuilder();
+        for (String ss : s) res.append(ss);
+        while (k < nums.length - 1 && res.charAt(k) == '0') k++;
+        return res.substring(k);
+    }
+}
+// @lc code=end
+
