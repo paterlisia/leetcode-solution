7d0337de7262db82fdd61cde2661a50ea19fd7d4
joanna <qiqiai77a@126.com>
2021-07-22 08:51:08 +0800

    finish java version of second practice: 189

---
+++ src/Java/practice/third-time/189.旋转数组.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ *
+ * [189] 旋转数组
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[] nums, int k) {
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k % nums.length - 1);
+        reverse(nums, k % nums.length, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = tmp;
+        }
+    }
+}
+// @lc code=end
+
