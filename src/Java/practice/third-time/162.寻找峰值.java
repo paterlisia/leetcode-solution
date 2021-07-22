6dedb3773e092f303d037c1947c6175b27b0c439
joanna <qiqiai77a@126.com>
2021-07-22 10:04:38 +0800

    finish java version of second practice: 162

---
+++ src/Java/practice/third-time/162.寻找峰值.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=java
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < nums[mid + 1]) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+}
+// @lc code=end
+
