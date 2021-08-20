3efce96002a7035b37896dc5fca6b883f493f2a0
joanna <qiqiai77a@126.com>
2021-08-21 03:06:29 +0800

    finish java version of second practice: 201

--- src/Java/practice/Second-time/4.寻找两个正序数组的中位数.java
+++ src/Java/practice/Second-time/4.寻找两个正序数组的中位数.java
@@ -15,7 +15,6 @@ class Solution {
         int len1 = end1 - start1 + 1;
         int len2 = end2 - start2 + 1;
         if (len1 > len2) recurr(nums2, nums1, start2, end2, start1, end1, k);
-        System.out.println(nums1.length);
         if (len1 == 0) return nums2[start2 + k - 1];
         if (k == 1) return Math.min(nums1[start1], nums2[start2]);
         int i = start1 + Math.min(len1, k / 2) - 1;
