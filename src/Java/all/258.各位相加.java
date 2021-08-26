a5fcae14f181e4a1e51455eb0426e14dcca12eb2
joanna <qiqiai77a@126.com>
2021-08-27 03:43:16 +0800

    finish java version of all: 258

---
+++ src/Java/all/258.各位相加.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=258 lang=java
+ *
+ * [258] 各位相加
+ */
+
+// @lc code=start
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}
+// @lc code=end
+
