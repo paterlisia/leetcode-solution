dc5356576dd96a5d3c4dd52d3b309f8e79628586
joanna <qiqiai77a@126.com>
2021-08-26 22:42:37 +0800

    finish java version of second practice: 242

---
+++ src/Java/practice/third-time/242.有效的字母异位词.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=java
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> smap=new HashMap<>();
+        int sl=s.length();
+        int tl=t.length();
+        if(sl!=tl){return false;}
+        for(int i=0;i<sl;i++){
+            smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
+            smap.put(t.charAt(i),smap.getOrDefault(t.charAt(i),0)-1);
+        }
+        for(char c:smap.keySet()){
+            if(smap.get(c)!=0){return false;}
+        }
+        
+        return true;
+    }
+}
+// @lc code=end
+
