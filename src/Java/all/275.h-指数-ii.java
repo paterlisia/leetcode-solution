ce1db8d4c54d48802a48864254136d29f765ecf6
joanna <qiqiai77a@126.com>
2021-08-27 21:32:29 +0800

    finish java version of all: 275

--- src/Java/all/275.h-指数-ii.java
+++ src/Java/all/275.h-指数-ii.java
@@ -7,7 +7,13 @@
 // @lc code=start
 class Solution {
     public int hIndex(int[] citations) {
-
+        int lo = 0, hi = citations.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (citations[mid] >= citations.length - mid) hi = mid - 1;
+            else lo = mid + 1; 
+        }
+        return citations.length - lo;
     }
 }
 // @lc code=end
