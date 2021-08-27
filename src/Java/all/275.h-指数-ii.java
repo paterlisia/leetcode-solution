61e06362f589ee47f9bc9bb3b0501e664c32026f
joanna <qiqiai77a@126.com>
2021-08-27 21:15:18 +0800

    finish java version of all: 274

---
+++ src/Java/all/275.h-指数-ii.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=275 lang=java
+ *
+ * [275] H 指数 II
+ */
+
+// @lc code=start
+class Solution {
+    public int hIndex(int[] citations) {
+
+    }
+}
+// @lc code=end
+
