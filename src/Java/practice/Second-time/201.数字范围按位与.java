65a54b017ba0f3fc4a359c53a3ebf2cb0c63aee3
joanna <qiqiai77a@126.com>
2021-08-29 02:53:38 +0800

    finish java version of all: 201

---
+++ src/Java/practice/Second-time/201.数字范围按位与.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=201 lang=java
+ *
+ * [201] 数字范围按位与
+ */
+
+// @lc code=start
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        return left << shift;
+    }
+}
+// @lc code=end
+
