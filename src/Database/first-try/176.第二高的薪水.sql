c8189837947cd67bc8e1f904f59770b35d0c741c
joanna <qiqiai77a@126.com>
2021-08-31 20:58:18 -0400

    finish mysql version of 177

---
+++ src/Database/first-try/176.第二高的薪水.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode.cn id=176 lang=mysql
+--
+-- [176] 第二高的薪水
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select ifnull((select distinct salary 
+from Employee 
+order by salary desc
+limit 1
+offset 1), null) as SecondHighestSalary
+
+-- @lc code=end
+
